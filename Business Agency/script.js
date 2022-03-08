742393ce75699c117d3d3ebfc809eccb2f2e77f4
godgod4661 <96277626+godgod4661@users.noreply.github.com>
2022-03-08 20:55:09 +0900

    update

---
+++ Business Agency/script.js
@@ -0,0 +1,40 @@
+const menu = document.querySelector('.menu');
+const navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click',() => {
+  navbar.classList.toggle('change');
+  menu.classList.toggle('change')
+})
+
+// End of Navbar
+
+// Section 2 Video
+const video = document.querySelector('.video')
+const btn = document.querySelector('.buttons i')
+const bar = document.querySelector('.video-bar')
+
+const playPause = () => {
+  if(video.paused) {
+    video.play()
+    btn.className = 'far fa-pause-circle'
+    video.style.opacity = '.7'
+  } else {
+    video.pause()
+    btn.className = 'far fa-play-circle'
+    video.style.opacity = '.3'
+  }
+}
+
+btn.addEventListener('click', () => {
+  playPause()
+})
+
+video.addEventListener('timeupdate',() => {
+  const barwidth = video.currentTime / video.duration
+  bar.style.width = `${barwidth * 100}%`
+  if(video.ended) {
+    btn.className = 'far fa-play-circle'
+    video.style.opacity = '.3'
+  }
+})
+// End of Section 2 Video
